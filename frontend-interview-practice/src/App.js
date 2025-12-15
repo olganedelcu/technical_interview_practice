dccf5afb3220c66d76fd9ee2aebd18d268897e23
olganedelcu <olganedelcuam@gmail.com>
2025-12-16 00:44:53 +0200

    search

--- frontend-interview-practice/src/App.js
+++ frontend-interview-practice/src/App.js
@@ -4,22 +4,29 @@ import React, {useEffect, useState} from 'react';
 function App() {  
   const [planets, setPlanets] = useState([]);
   const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
   
  useEffect( () => {
   async function fetchData() {
-    const response = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
+    const response = await fetch(`https://swapi.dev/api/planets/?page=${page}&search=${search}`);
     const data = await response.json();
     console.log(data);
     setPlanets(data.results);
   }
   fetchData();
-  }, [page]);
+  }, [page, search]);
 
 
   return (
     <div className="App">
       <h1>Frontend Interview Practice</h1>
       <p>Fetch and display data here</p>
+      <input 
+        type="text" 
+        placeholder="Search planets..." 
+        value={search} 
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <div>{planets.map((planet, index) => 
         {return <p key={index}>{planet.name}</p>})}</div>
       <button onClick={() => setPage(page - 1)} disabled={page === 1}>Previous</button>
