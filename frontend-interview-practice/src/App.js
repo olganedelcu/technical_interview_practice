850f69f0c20a0dde4c0221567a2b25b36f327359
olganedelcu <olganedelcuam@gmail.com>
2025-12-16 00:46:38 +0200

    git push

--- frontend-interview-practice/src/App.js
+++ frontend-interview-practice/src/App.js
@@ -1,38 +1,54 @@
-import './App.css';
-import React, {useEffect, useState} from 'react';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
-function App() {  
+function App() {
   const [planets, setPlanets] = useState([]);
   const [page, setPage] = useState(1);
-  const [search, setSearch] = useState('');
-  
- useEffect( () => {
-  async function fetchData() {
-    const response = await fetch(`https://swapi.dev/api/planets/?page=${page}&search=${search}`);
-    const data = await response.json();
-    console.log(data);
-    setPlanets(data.results);
-  }
-  fetchData();
-  }, [page, search]);
+  const [search, setSearch] = useState("");
 
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://swapi.dev/api/planets/?page=${page}&search=${search}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setPlanets(data.results);
+    }
+    fetchData();
+  }, [page, search]);
 
   return (
     <div className="App">
       <h1>Frontend Interview Practice</h1>
       <p>Fetch and display data here</p>
-      <input 
-        type="text" 
-        placeholder="Search planets..." 
-        value={search} 
+      <input
+        type="text"
+        placeholder="Search planets..."
+        value={search}
         onChange={(e) => {
           setSearch(e.target.value);
           setPage(1);
         }}
       />
-      <div>{planets.map((planet, index) => 
-        {return <p key={index}>{planet.name}</p>})}</div>
-      <button onClick={() => setPage(page - 1)} disabled={page === 1}>Previous</button>
+      <div>
+        {planets.map((planet, index) => {
+          return (
+            <p key={index}>
+              {planet.name}
+              <p>Climate: {planet.climate}</p>
+              <p>Terrain: {planet.terrain}</p>
+              <p>
+                Population:
+                {planet.population}
+              </p>
+            </p>
+          );
+        })}
+      </div>
+      <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+        Previous
+      </button>
       <button onClick={() => setPage(page + 1)}>Next</button>
     </div>
   );
